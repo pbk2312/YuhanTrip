0c76b6df8421a2b0ff6902a2b19161e6d34b414e
Park Yu Han <pbk2312@naver.com>
2024-07-13 23:27:12 +0900

    .

--- src/main/java/hello/yuhanTrip/controller/AccommodationController.java
+++ src/main/java/hello/yuhanTrip/controller/AccommodationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
-
+//
 @RestController
 public class AccommodationController {
 
