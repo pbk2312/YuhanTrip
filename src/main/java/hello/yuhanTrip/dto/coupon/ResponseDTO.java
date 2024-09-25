be5e9d2f5ae2168675c13065e7b2d0bfe0b83600
Park Yu Han <pbk2312@naver.com>
2024-09-25 15:00:45 +0900

    중복 제거를 위한 응답 DTO 작성

---
+++ src/main/java/hello/yuhanTrip/dto/coupon/ResponseDTO.java
@@ -0,0 +1,12 @@
+package hello.yuhanTrip.dto.coupon;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDTO<T> {
+    private String message; // 응답 메시지
+    private T data;         // 응답 데이터 (제네릭 타입으로 다양한 데이터 유형을 처리 가능)
+}
